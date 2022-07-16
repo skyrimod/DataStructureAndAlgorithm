e21a9ad7587678ea7eb89ef1bb8800f6d6ccefa2
jindejun <732273586@qq.com>
2022-07-16 23:29:36 +0800

    create new project

---
+++ cmake-build-debug/CMakeFiles/DataStructureAndAlgorithm.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/DataStructureAndAlgorithm.dir/main.c.obj"
+  "DataStructureAndAlgorithm.exe"
+  "DataStructureAndAlgorithm.exe.manifest"
+  "DataStructureAndAlgorithm.pdb"
+  "libDataStructureAndAlgorithm.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/DataStructureAndAlgorithm.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ cmake-build-debug/CMakeFiles/DataStructureAndAlgorithm.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for DataStructureAndAlgorithm.
+# This may be replaced when dependencies are built.
